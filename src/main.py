21aea68d4831c545ec3e530673266fc6ecfaa592
Dan Nicholls <dannicholls12@gmail.com>
2024-06-01 22:05:01 +1000

    feat(main): Add support for pause and reset

--- src/main.py
+++ src/main.py
@@ -33,6 +33,7 @@ def main():
     setup_screen(screen, settings)
 
     step_count = 0
+    is_paused = True
 
     running = True
     while running:
@@ -42,10 +43,21 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+                elif event.key == pygame.K_r:
+                    is_paused = True
+                    values = generate_initial_values(30)
+                    algorithm.reset(values)
+                    step_count = 0
+                    setup_screen(screen, settings)
+                    continue
+                elif event.key == pygame.K_SPACE:
+                    is_paused = not is_paused
+                    continue
 
-        if not algorithm.is_sorted():
-            algorithm.step()
-            step_count += 1
+        if not is_paused:
+            if not algorithm.is_sorted():
+                algorithm.step()
+                step_count += 1
 
         highlight_indexes = (
             [algorithm.j, algorithm.j + 1]
